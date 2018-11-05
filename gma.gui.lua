9f5eb19610ec26599adc5e76c64c18741a4a3e3a
Andrea Bergamasco <abergamasco@gmail.com>
2018-11-05 21:25:18 +0100

    Fixed gma.gui.confirm version lock and added gma.gui.msgbox
    gma.gui.confirm did not work only in version 3.1.2.5

--- gma.gui.lua
+++ gma.gui.lua
@@ -1,9 +1,8 @@
 ---GUI-related GMA LUA functions
 --@module gma.gui
 
----REMOVED.
 --
---WARNING!!!  Calling this function on a version >= 3.1.2.5 may hang your console!
+--WARNING!!!  Calling this function on version 3.1.2.5 may hang your console!
 --
 --Prompt user to confirm.
 --
@@ -11,5 +10,13 @@
 --Removed as of GMA v3.1.2.5.
 --@tparam string title Title of the prompt.
 --@tparam string message Message of the prompt.
+--@treturn boolean true when clicking ok, nil otherwise
 function gma.gui.confirm(title,message)
+end
+
+--Show a notice to user
+--
+--@tparam string title Title of the message box.
+--@tparam string message Message of the notice.
+function gma.gui.msgbox(title,message)
 end
