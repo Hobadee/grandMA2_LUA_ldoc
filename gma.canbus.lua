f64f67d58f4a2737227a816181bef3c2f28a3626
Andrea Bergamasco <abergamasco@gmail.com>
2018-12-02 14:43:51 +0100

    Stubbed gma.canbus
    Added gma.canbus.hardkey as a stub, needs more digging.

---
+++ gma.canbus.lua
@@ -0,0 +1,10 @@
+---Canbus related GMA LUA functions
+--@module gma.canbus
+
+---Emulate an hardkey press ??
+--
+--@tparam integer keycode numeric code corresponding to a key (106 = Please, 54 = Esc)
+--@tparam boolean option_a ?? default=true
+--@treturn boolean option_b ?? default=false
+function gma.canbus.hardkey(keycode, option_a, option_b)
+end
