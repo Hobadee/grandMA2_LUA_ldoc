08d48ca6f44bd9d42d0984f4b38483ef78b6deac
Andrea Bergamasco <abergamasco@gmail.com>
2018-12-02 22:27:35 +0100

    Updated gma.canbus.hardkey params

--- gma.canbus.lua
+++ gma.canbus.lua
@@ -3,8 +3,8 @@
 
 ---Emulate an hardkey press ??
 --
---@tparam integer keycode numeric code corresponding to a key (106 = Please, 54 = Esc)
---@tparam boolean option_a ?? default=true
---@treturn boolean option_b ?? default=false
-function gma.canbus.hardkey(keycode, option_a, option_b)
+--@tparam number code numeric code corresponding to a key (106 = Please, 54 = Esc)
+--@tparam boolean pressed ?? default=true
+--@treturn boolean hold ?? default=false
+function gma.canbus.hardkey(code, pressed, hold)
 end
