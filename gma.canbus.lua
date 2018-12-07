2e2a0bfb397a8f682839247255f4af727c369ed2
Andrea Bergamasco <abergamasco@gmail.com>
2018-12-07 23:40:41 +0100

    Added warning and discovered codes
    Added warning and discovered codes

--- gma.canbus.lua
+++ gma.canbus.lua
@@ -1,10 +1,113 @@
 ---Canbus related GMA LUA functions
 --@module gma.canbus
 
----Emulate an hardkey press ??
+--WARNING!! This is a low-level function and might have unpredictable and harmful consequences on your console and/or showfile.
+-- Use it at your own risk.
+--
+---Emulate a hardkey press
 --
 --@tparam number code numeric code corresponding to a key (106 = Please, 54 = Esc)
---@tparam boolean pressed ?? default=true
---@treturn boolean hold ?? default=false
+--@tparam boolean pressed
+--@treturn boolean hold
+--
+-- Codes discovered so far:
+--
+--   9: Macro: DefGoPause
+--  10: Macro: DefGoBack
+--  11: Macro: DefGoForward
+--  12: TA_X0  pressed=1   ViewButton 11.1
+--  13: TA_X1  pressed=1   ViewButton 11.2
+--  14: TA_X2  pressed=1   ViewButton 11.3
+--  15: TA_X3  pressed=1   ViewButton 11.4
+--  16: TA_X4  pressed=1   ViewButton 11.5
+--  17: TA_X5  pressed=1   ViewButton 11.6
+--  18: TA_X6  pressed=1   ViewButton 11.7
+--  19: TA_X7  pressed=1   ViewButton 11.8
+--  20: TA_X8  pressed=1   ViewButton 11.9
+--  21: TA_X9  pressed=1   VIewButton 11.10
+--  22: TA_X10 pressed=1   VIewButton 11.11
+--  23: TA_X11 pressed=1   VIewButton 11.12
+--  24: TA_X12 pressed=1   VIewButton 11.13
+--  25: TA_X13 pressed=1   VIewButton 11.14
+--  26: TA_X14 pressed=1   VIewButton 11.15
+--  27: TA_X15 pressed=1   VIewButton 11.16
+--  28: TA_X16 pressed=1   VIewButton 11.17
+--  29: TA_X17 pressed=1   VIewButton 11.18
+--  30: TA_X18 pressed=1   VIewButton 11.19
+--  31: TA_X19 pressed=1   ViewButton 11.20
+--  40: ⦿ ("Nipple")
+--  41: Fix
+--  42: Select
+--  43: Off
+--  44: Temp
+--  45: Top
+--  46: On
+--  47: <<<
+--  48: Learn
+--  49: >>>
+--  50: Go-
+--  51: Pause
+--  52: Go
+--  54: Esc
+--  55: Edit
+--  56: Goto
+--  57: Update
+--  58: Time
+--  59: Store
+--  61: Freeze
+--  62: Preview
+--  63: Assign
+--  64: Align
+--  65: B.O. (Blackout)
+--  66: View
+--  67: Effect
+--  68: MA
+--  69: Delete
+--  70: Page
+--  71: Macro
+--  72: Preset
+--  73: Copy
+--  74: Sequence
+--  75: Cue
+--  76: Executor
+--  82: Channel
+--  83: Fixture
+--  84: Group
+--  85: Move
+--  86: 0
+--  87: 1
+--  88: 2
+--  89: 3
+--  90: 4
+--  91: 5
+--  92: 6
+--  93: 7
+--  94: 8
+--  95: 9
+--  96: +
+--  97: -
+--  98: .
+--  99: Full
+-- 100: Highlight
+-- 101: Solo
+-- 102: Thru
+-- 103: If
+-- 104: At
+-- 106: Please
+-- 116: Help
+-- 117: Backup
+-- 118: Setup
+-- 119: Tools
+-- 120: ViewButton 1.1 /screen=2
+-- 121: ViewButton 1.2 /screen=2
+-- 122: ViewButton 1.3 /screen=2
+-- 123: ViewButton 1.4 /screen=2
+-- 124: ViewButton 1.5 /screen=2
+-- 125: ViewButton 1.6 /screen=2
+-- 126: ViewButton 1.7 /screen=2
+-- 127: ViewButton 1.8 /screen=2
+-- 128: ViewButton 1.9 /screen=2
+-- 129: ViewButton 1.10 /screen=2
+-- 155 and higher: Macro DefGoPause
 function gma.canbus.hardkey(code, pressed, hold)
 end
